7dfaed9741b4ff1bc96b3631670d6587d7b9aaf4
Sumudu Jayasinghe <sumudu@wso2telco.com>
2017-01-27 15:05:49 +0530

    add subscription creations detail view

--- src/app/approvals/subscriptions/subscriptions.component.ts
+++ src/app/approvals/subscriptions/subscriptions.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApplicationTask} from "../../commons/models/application-data-models";
+import {ApprovalRemoteDataService} from "../../data-providers/approval-remote-data.service";
+import {MessageService} from "../../commons/services/message.service";
+import {error} from "util";
 
 @Component({
-  selector: 'app-subscriptions',
-  templateUrl: './subscriptions.component.html',
-  styleUrls: ['./subscriptions.component.scss']
+    selector: 'app-subscriptions',
+    templateUrl: './subscriptions.component.html',
+    styleUrls: ['./subscriptions.component.scss']
 })
 export class SubscriptionsComponent implements OnInit {
 
-  constructor() { }
+    private mySubscriptions: ApplicationTask[];
+    private allSubscriptions: ApplicationTask[];
 
-  ngOnInit() {
-  }
+    constructor(private message: MessageService,
+                private approvalService: ApprovalRemoteDataService) {
+    }
+
+    ngOnInit() {
+        this.approvalService.MySubscriptionTasksProvider.subscribe(
+            (subs) => {
+                this.mySubscriptions = subs
+            },
+            (error) => {
+                this.message.error(error)
+            }
+        );
+
+        this.approvalService.GroupSubscriptionTasksProvider.subscribe(
+            (subs) => {
+                this.allSubscriptions = subs
+            },
+            (error) => {
+                this.message.error(error)
+            }
+        );
+
+        this.approvalService.getUserAppSubscriptionTasks();
+        this.approvalService.getUserGroupAppSubscriptionTask();
+    }
 
 }
