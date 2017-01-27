f35e914e8e3655cc72a89c2fad8bd5d8d613173c
Sumudu Jayasinghe <sumudu@wso2telco.com>
2017-01-27 14:49:32 +0530

    add application creation detail view

--- src/app/approvals/applications/applications.component.ts
+++ src/app/approvals/applications/applications.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApplicationTask} from "../../commons/models/application-data-models";
+import {ApprovalRemoteDataService} from "../../data-providers/approval-remote-data.service";
+import {MessageService} from "../../commons/services/message.service";
 
 @Component({
-  selector: 'app-applications',
-  templateUrl: './applications.component.html',
-  styleUrls: ['./applications.component.scss']
+    selector: 'app-applications',
+    templateUrl: './applications.component.html',
+    styleUrls: ['./applications.component.scss']
 })
 export class ApplicationsComponent implements OnInit {
 
-  constructor() { }
+    private myApplications: ApplicationTask[];
+    private allApplications: ApplicationTask[];
 
-  ngOnInit() {
-  }
+    constructor(private message: MessageService,
+                private approvalService: ApprovalRemoteDataService) {
+    }
+
+    ngOnInit() {
+        this.approvalService.MyApplicationCreationTasksProvider.subscribe(
+            (apps) => {
+                this.myApplications = apps;
+            },
+            (error) => {
+                this.message.error(error);
+            }
+        );
+
+        this.approvalService.GroupApplicationCreationTasksProvider.subscribe(
+            (apps) => {
+                this.allApplications = apps;
+            },
+            (error) => {
+                this.message.error(error)
+            }
+        );
+
+        this.approvalService.getUserApplicationTasks();
+        this.approvalService.getUserGroupApplicationTasks();
+    }
 
 }
--- src/app/dashboard/approval-summery/approval-summery.component.ts
+++ src/app/dashboard/approval-summery/approval-summery.component.ts
@@ -1,19 +1,29 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {DashboardData} from "../../commons/models/dashboard-data-models";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-approval-summery',
-  templateUrl: './approval-summery.component.html',
-  styleUrls: ['./approval-summery.component.scss'],
+    selector: 'app-approval-summery',
+    templateUrl: './approval-summery.component.html',
+    styleUrls: ['./approval-summery.component.scss'],
 })
 export class ApprovalSummeryComponent implements OnInit {
 
-  @Input()
-  private appDetailsSummery:DashboardData;
+    @Input()
+    private appDetailsSummery: DashboardData;
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onCountClick(type: string) {
+        if (type === 'APPLICATIONS') {
+            this.router.navigate(['/approvals/applications'])
+        } else if (type === 'SUBSCRIPTIONS') {
+            this.router.navigate(['/approvals/subscriptions'])
+        }
+    }
 
 }
