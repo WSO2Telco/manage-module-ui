3d418e19b95c39199178670780b2a6d20595c0de
sahantelco <sahan@wso2telco.com>
2017-09-05 19:14:59 +0530

    Adding Assign rate for API operation UI

---
+++ src/app/rate/assign/assign-rate-main.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-assign-rate-main',
+    templateUrl: './assign-rate-main.component.html',
+    styleUrls: ['./assign-rate.component.scss']
+})
+
+export class AssignRateMainComponent implements OnInit {
+
+    ngOnInit(): void {
+    }
+
+}
