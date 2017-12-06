9724b643629bab2108e6097cb98e503f965d4e50
shawijayasekera <shashika@wso2telco.com>
2017-12-06 16:37:22 +0530

    MA-2
    Add changes to return JSESSIONID when user login

--- src/main/java/com/wso2telco/dep/manageservice/resource/resource/authentication/AuthenticationResource.java
+++ src/main/java/com/wso2telco/dep/manageservice/resource/resource/authentication/AuthenticationResource.java
@@ -1,6 +1,8 @@
 package com.wso2telco.dep.manageservice.resource.resource.authentication;
 
+import com.wso2telco.core.authfilter.util.HeaderParam;
 import com.wso2telco.dep.manageservice.resource.service.authentication.AuthenticationService;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,6 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 @Path("/authentication")
@@ -19,12 +22,13 @@ public class AuthenticationResource {
 
     @GET
     @Path("/login")
-    public Response setApp(@Context HttpHeaders headers) {
+    public Response setApp(@Context HttpHeaders headers, @Context HttpServletRequest request) {
     	
-    	String userName = headers.getRequestHeader("user-name").get(0);
+    	String userName = headers.getRequestHeader(HeaderParam.USER_NAME.getTObject()).get(0);
     	
         Object responseString = null;
         responseString = authenticationService.doLogin(userName);
-        return Response.status(Response.Status.OK).entity(responseString).build();
+        String sessionId = request.getSession(true).getId();
+        return Response.status(Response.Status.OK).entity(responseString).cookie(new NewCookie("JSESSIONID", sessionId)).build();
     }
 }
