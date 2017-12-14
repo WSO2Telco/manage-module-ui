a6fe066422269547446f96c1611d0514a7c7c6a8
Nuwan Walisundara <nuwan@wso2telco.com>
2017-12-14 20:19:46 +0530

    CO-8

--- src/main/java/com/wso2telco/dep/manageservice/resource/service/authentication/AuthenticationService.java
+++ src/main/java/com/wso2telco/dep/manageservice/resource/service/authentication/AuthenticationService.java
@@ -8,6 +8,8 @@ import com.wso2telco.core.userprofile.cache.CacheFactory;
 import com.wso2telco.core.userprofile.cache.UserProfileCachable;
 import com.wso2telco.core.userprofile.dto.UserProfileDTO;
 import com.wso2telco.core.userprofile.util.CacheType;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class AuthenticationService {
@@ -16,8 +18,9 @@ public class AuthenticationService {
 
 		UserProfileRetriever userProfileRetriever = new UserProfileRetriever();
 		UserProfileDTO userProfileDTO = userProfileRetriever.getUserProfile(userName);
-		Map<String, Object> returnJson = userProfileDTO.getUiPermissions();
-		
+		Map<String, Object> permissionTree = userProfileDTO.getUiPermissions();
+		Map<String, Object> returnJson = new HashMap<>();
+		returnJson.put("permissions", permissionTree);
 		returnJson.put("userName", userProfileDTO.getUserName());
 		returnJson.put("organization", userProfileDTO.getOrganization());
 		returnJson.put("Department",userProfileDTO.getDepartment());
