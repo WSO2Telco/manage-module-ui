d1d4fa5ff13f0d04e2bfc678ab601f0718e5b2e6
Gayantha Samaradiwakara <gayantha.samaradiwakara@apigate.com>
2020-04-09 19:58:39 +0530

    Form fixes (removed "ngNoForm")

--- component/manage-ui/src/app/rate/rate.module.ts
+++ component/manage-ui/src/app/rate/rate.module.ts
@@ -1,19 +1,19 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RateCategoryComponent} from './create/rate-ratecategory/ratecategory.component';
-import {CategoryComponent} from './create/rate-category/category.component';
-import {TariffComponent} from './create/rate-tariff/tariff.component';
-import {TaxComponent} from './create/rate-tax/tax.component';
-import {RateMainComponent} from './create/rate-main/rate-main.component';
-import {AssignRateMainComponent} from './assign/assign-rate-main.component';
-import {ViewRateMainComponent} from './view/view-rate-main.component';
-import {RateRoutes} from './rate.routes';
-import {SharedModule} from '../shared/shared.module';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RateCategoryComponent } from './create/rate-ratecategory/ratecategory.component';
+import { CategoryComponent } from './create/rate-category/category.component';
+import { TariffComponent } from './create/rate-tariff/tariff.component';
+import { TaxComponent } from './create/rate-tax/tax.component';
+import { RateMainComponent } from './create/rate-main/rate-main.component';
+import { AssignRateMainComponent } from './assign/assign-rate-main.component';
+import { ViewRateMainComponent } from './view/view-rate-main.component';
+import { RateRoutes } from './rate.routes';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import {TooltipModule} from 'ngx-bootstrap/tooltip';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
-import {CurrencyComponent} from './create/rate-currency/currency.component';
+import { CurrencyComponent } from './create/rate-currency/currency.component';
 import { AngularDualListBoxModule } from 'angular-dual-listbox';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
@@ -30,7 +30,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
         TooltipModule.forRoot(),
         TabsModule.forRoot(),
         CollapseModule.forRoot(),
-        AngularDualListBoxModule,
+        AngularDualListBoxModule
     ],
     declarations: [
         RateMainComponent,
