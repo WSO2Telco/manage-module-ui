36aaaf50f4f60117fe4b4cbf0aeccf886b941a02
Sahan Kariyawasam <sahan@wso2telco.com>
2020-10-21 20:18:54 +0530

    Update edit tier routes and module

--- component/manage-ui/src/app/subscriptions/edit-subscription.module.ts
+++ component/manage-ui/src/app/subscriptions/edit-subscription.module.ts
@@ -1,13 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EditSubscriptionRoutes} from './edit-subscription.routes';
-import {SubscriptionDetailComponent} from './edit-subscription/subscription-detail.component';
-import {UpdateSubComponent} from './subscription-detail/update-sub.component';
-import {ApplicationDataTableComponent} from "../commons/components/application-data-table/application-data-table.component";
+import {SubscriptionDetailComponent} from './subscription-detail/subscription-detail.component';
 import {SharedModule} from "../shared/shared.module";
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import {TooltipModule} from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { TierEditComponent } from './tier-edit/tier-edit.component';
 
 @NgModule({
     imports: [
@@ -21,7 +20,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     ],
     declarations: [
         SubscriptionDetailComponent,
-        UpdateSubComponent
+        TierEditComponent
         ]
 })
 export class EditSubscriptionModule {
--- component/manage-ui/src/app/subscriptions/edit-subscription.routes.ts
+++ component/manage-ui/src/app/subscriptions/edit-subscription.routes.ts
@@ -1,24 +1,11 @@
 import { RouterModule } from "@angular/router";
-import { SubscriptionDetailComponent } from "./edit-subscription/subscription-detail.component";
-import { UpdateSubComponent } from "./subscription-detail/update-sub.component";
+import { SubscriptionDetailComponent } from "./subscription-detail/subscription-detail.component";
 import { PermissionGuard } from "app/app.guard";
 
 const routes = [
   {
     path: '',
     component: SubscriptionDetailComponent
-  },
-  {
-    path: ':appid/:apiname/:apiversion',
-    component: UpdateSubComponent
-  },
-  {
-    path: ':appid/:apiname/:appname/:apiversion/:apiprovider/:tier/:action/:status/:operator',
-    component: UpdateSubComponent
-  },
-  {
-    path: ':appid/:apiname/:tier/:action/:status/:operator',
-    component: UpdateSubComponent
   }
 ];
 
